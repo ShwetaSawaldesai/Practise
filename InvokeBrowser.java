2b025f73b4f293c10f8a5b21f84dabb9f56dd519
ShwetaSawaldesai <shweta8828@gmail.com>
2018-10-30 16:09:15 +0530

    Invoke browser file is added

---
+++ InvokeBrowser.java
@@ -0,0 +1,27 @@
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SignUP {
+
+	static WebDriver driver;
+
+	@Test()
+	public void InvokeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\selenium 64 bit\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.get("http://staging.aceinvoice.com/sign_up");
+
+	}}
